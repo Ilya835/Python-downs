f82e5e985fba846613bb1addfa68acd509879881
Илья <comrad.ivan.the.best.comrad@gmail.com>
2022-07-16 07:46:32 +0300

    соси хуй

---
+++ Main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+class MainFrame(Frame):
+    def __init__(self):
+        self.initUI()
+
+    def initUI(self):
+        self.master.title("Соси хуй")
+        self.pack(fill=BOTH, expand=1)
+
+        canvas = Canvas(self)
+        canvas.create_rectangle()
