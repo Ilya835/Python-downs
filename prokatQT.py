5b55ecbb5e793a4141c1767f1aac8cdac2f86637
Илья Иванов <comrad.ivan.the.best.comrad@gmail.com>
2024-01-10 04:53:24 +0300

    Перенос методов из Tk клиента

--- prokatQT.py
+++ prokatQT.py
@@ -2,6 +2,14 @@ from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt, QAbstractTableModel, QModelIndex
 from form import Ui_MainWindow  # импорт нашего сгенерированного файла
 import sys
+import csv
+
+#Массив отвечающий за журнал проката
+stat = []
+#Массив отвечающий за велосипеды
+velos = []
+#Массив отвечающий за базу данных клиентов
+client = []
 
 class MatrixTableModel(QAbstractTableModel):
     def __init__(self, data):
@@ -31,13 +39,53 @@ class MatrixTableModel(QAbstractTableModel):
         return True
 
 class mywindow(QtWidgets.QMainWindow):
+
+    #Вывод отладочных значений журнала проката в консоль
+    def return_arr():
+        print(stat)
+
     def __init__(self):
+            #Сохранение значений массива в файл CSV
+        def save_array_to_csv(array, filename):
+            with open(filename, 'w', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerows(array)
+
+    #Чтение значение журнала проката из файла stat.csv
+        def read_array_from_csv(filename):
+            with open(filename, 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    stat.append(row)
+
+    #Чтение значение таблицы велосипедов из файла velos.csv
+        def read_array_from_csv2(filename):
+            with open(filename, 'r') as file:
+                reader = csv.reader(file)
+                for row in reader:
+                    velos.append(row)
+
+         #Обновление значений из файлов
+        def update():
+            #Очистка массивов для велосипедов и журала проката
+            velos.clear()
+            stat.clear()
+            #Считывание данных из файлов и запись их в массивы
+            read_array_from_csv('stat.csv')
+            read_array_from_csv2('velos.csv')
+            self.velosi = MatrixTableModel(velos)
+            self.stati = MatrixTableModel(stat)
+            self.ui.tableView.setModel(self.stati)
+            #Отладочный вывод значений массивов в консоль
+            print(stat)
+            print(velos)
+
         super(mywindow, self).__init__()
-        velos = [[1,2,3,4],[4,3,2,1]]
-        self.model = MatrixTableModel(velos)
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.tableView.setModel(self.model)
+        update()
+
 
 app = QtWidgets.QApplication([])
 application = mywindow()
